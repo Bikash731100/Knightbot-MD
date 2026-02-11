911e98ac8f7fe14aa384cea783d980fa9c258c30
Bikash731100 <165351009+Bikash731100@users.noreply.github.com>
2026-02-11 15:44:26 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -238,7 +238,7 @@ Join our channel for updates:`;
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterName: 'Nobita ',
                         serverMessageId: -1
                     }
                 }
@@ -252,7 +252,7 @@ Join our channel for updates:`;
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD by Mr Unique Hacker',
+                        newsletterName: 'Nobita ',
                         serverMessageId: -1
                     } 
                 }
